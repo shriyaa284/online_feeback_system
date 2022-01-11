2d7ad6cdc3b19a2dd837b9693e43777e69b307ae
Shriya Singh <76433303+shriyaa284@users.noreply.github.com>
2022-01-11 17:53:44 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,28 @@
+
+
+    <!-- Navigation -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header" align="center"><font color="#00FF33">ONLINE FACULTY FEEDBACK SYSTEM</font> </h1>
+                
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Intro Content -->
+        <div class="row" style="margin-bottom:50px;margin-left:50px">
+           
+               <P>Student Feedback system for College
+Here we have developed the a faculty feedback system, which is generally used in the college to rate the faculty based on the performance...Here we have 2 modules such as administrator, student.
+
+Administrator is the one who creates the student account by adding all student info and assigning the username and password. 
+Admin als0 checks the result once all students entered the feedback..
+
+</P> 
+               
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Footer -->
+     
